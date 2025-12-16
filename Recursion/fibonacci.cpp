df3ef0edd2f61bef23186511a9f013bd12d2bb58
IshanBaidya <165746811+VaidyaIshan@users.noreply.github.com>
2025-12-16 10:24:23 +0545

    Fibonacci

--- Recursion/fibonacci.cpp
+++ Recursion/fibonacci.cpp
@@ -1,30 +1,35 @@
-#include<stdio.h>
-int stack[100];
-int fact(int n){
-    int i=0;
-    
-    if(n==1 || n==0){
-        printf("\nBase Condition in Recursion\t");
-        stack[i]=n;
-        printf("%d*",stack[i]);
-        i=i+1;
+#include <stdio.h>
+
+int fibonacci(int n) {
+    // Base cases: stop the recursion
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
         return 1;
-        
     }
-    else
-    {   printf("\nRecursive Condition in Recursion\t");
-        stack[i]=n;
-        printf("%d*",stack[i]);
-        i=i+1;
-        return n*fact(n-1);
+
+    else {
+        return (fibonacci(n - 1) + fibonacci(n - 2));
     }
 }
-int main(){
-    int n,result,i;
-    printf("\n Enter a number to find factorial:");
-    scanf("%d",&n);
-    printf("\nElements in Stack:");
-    result=fact(n);
-    printf("\n%d is the factorial of %d\n",result,n);
-    
+
+int main() {
+    int n, i;
+
+  
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Number of terms cannot be negative.\n");
+    } else {
+        printf("Fibonacci Series: ");
+       
+        for (i = 0; i < n; i++) {
+            printf("%d ", fibonacci(i));
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
